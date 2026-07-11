ebf00a141f1c04ac94f9582802a7de07bcd5efe6
docssam1 <docssam1@gmail.com>
2026-07-11 13:15:28 +0900

    chore: 미디어쿼리 전환으로 불필요해진 nm-townmode 클래스 토글 제거

--- number_magic/app/main.js
+++ number_magic/app/main.js
@@ -124,7 +124,6 @@ function startAmbience(){
 let townCleanup=null;
 function render(){
   if(townCleanup){townCleanup();townCleanup=null;}
-  app.classList.toggle('nm-townmode',S.view==='town');
   app.innerHTML=`<div class="nm-top">
     <div class="nm-brand">🪄 ${t('appName')}</div>
     <div class="nm-top-right">
