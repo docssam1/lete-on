f7cf32b33b3ddbfc199a81cbb91254e4184620c9
docssam1 <docssam1@users.noreply.github.com>
2026-08-02 19:50:44 +0900

    feat(fields): replace mock exam viewer pages

--- fields-classic/print-viewer/documents.js
+++ fields-classic/print-viewer/documents.js
@@ -16,43 +16,43 @@ window.GFIELD_PRINT_DOCUMENTS = {
   },
   "mock-set": {
     title: "필즈 더 클래식 6세 선발 모의고사 SET",
-    pageCount: 76,
+    pageCount: 63,
     pagesBase: "../assets/print/mock-set/page_"
   },
   "mock-set-1": {
     title: "필즈 더 클래식 실전 모의고사 1회",
     startPage: 1,
-    pageCount: 15,
+    pageCount: 13,
     pagesBase: "../assets/print/mock-set/page_"
   },
   "mock-set-2": {
     title: "필즈 더 클래식 실전 모의고사 2회",
-    startPage: 16,
+    startPage: 14,
     pageCount: 13,
     pagesBase: "../assets/print/mock-set/page_"
   },
   "mock-set-3": {
     title: "필즈 더 클래식 실전 모의고사 3회",
-    startPage: 29,
-    pageCount: 14,
+    startPage: 27,
+    pageCount: 9,
     pagesBase: "../assets/print/mock-set/page_"
   },
   "mock-set-4": {
     title: "필즈 더 클래식 실전 모의고사 4회",
-    startPage: 43,
-    pageCount: 11,
+    startPage: 36,
+    pageCount: 9,
     pagesBase: "../assets/print/mock-set/page_"
   },
   "mock-set-5": {
     title: "필즈 더 클래식 실전 모의고사 5회",
-    startPage: 54,
-    pageCount: 11,
+    startPage: 45,
+    pageCount: 9,
     pagesBase: "../assets/print/mock-set/page_"
   },
   "mock-set-6": {
     title: "필즈 더 클래식 실전 모의고사 6회",
-    startPage: 65,
-    pageCount: 12,
+    startPage: 54,
+    pageCount: 10,
     pagesBase: "../assets/print/mock-set/page_"
   }
 };
