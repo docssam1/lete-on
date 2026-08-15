b3663944a03a690b70b7a6000705d3603680a79d
docssam1 <docssam1@users.noreply.github.com>
2026-08-16 07:23:58 +0900

    feat(fields): verify selection test 6 question 15

--- fields-classic/question-bank/app.js
+++ fields-classic/question-bank/app.js
@@ -1,4 +1,4 @@
-import { AGE_STAGES, DOMAINS, TYPES, EXAMS, PRACTICE_EXAM_TYPES, FINAL_EXAM_TYPES, CURRICULUM, typeById } from "./source-data.js?v=20260816dd";
+import { AGE_STAGES, DOMAINS, TYPES, EXAMS, PRACTICE_EXAM_TYPES, FINAL_EXAM_TYPES, CURRICULUM, typeById } from "./source-data.js?v=20260816de";
 import { GENERATORS } from "./generators.js?v=20260816cq";
 
 const $ = (id) => document.getElementById(id);
