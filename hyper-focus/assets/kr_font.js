8b669f226dc2a326782efee518bfdd1937e3ad71
docssam1 <docssam1@gmail.com>
2026-08-18 01:19:55 +0900

    fix(hf): bust q01 solution helper cache

--- hyper-focus/assets/kr_font.js
+++ hyper-focus/assets/kr_font.js
@@ -1,3 +1,3 @@
 /* Hyper Focus bootstrap: preserve the original Korean PDF font payload and load visual solution helpers. */
 document.write('<script src="./assets/kr_font_data.js"><\/script>');
-document.write('<script src="./assets/hf_solution_visuals.js"><\/script>');
+document.write('<script src="./assets/hf_solution_visuals.js?v=20260818b"><\/script>');
